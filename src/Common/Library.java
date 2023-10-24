d43d5fed44e29efddfea4bfc2e1f2a9efb089f07
Long Nguyễn <huylong.tk123@gmail.com>
2023-10-24 12:33:28 +0700

    lab211 by Long

---
+++ src/Common/Library.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Common;
+
+import java.util.Scanner;
+
+public class Library {
+   
+    Scanner sc = new Scanner(System.in);
+    
+    public int inputIntLimit(int min, int max) {
+        while (true) {
+            try {
+                int result = Integer.parseInt(sc.nextLine().trim());
+                if (result < min || result > max) {
+                    throw new NumberFormatException();
+                }
+                return result;
+            } catch (NumberFormatException e) {
+                System.err.println("Please input number in rage [" + min + ", " + max + "]");
+                System.out.print("Enter again: ");
+            }
+        }
+    }
+
+    public  String checkInputString() {
+        //loop until user input true value
+        while (true) {
+            String result = sc.nextLine().trim();
+            if (result.isEmpty()) {
+                System.err.println("Not empty.");
+            } else {
+                return result;
+            }
+        }
+    }
+  
+}
---
+++ src/Model/Data.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+public class Data {
+    private String inputString;
+
+    public Data() {
+    }
+
+    public Data(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public String getInputString() {
+        return inputString;
+    }
+
+    public void setInputString(String inputString) {
+        this.inputString = inputString;
+    }
+    
+    
+}
